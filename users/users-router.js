1b93a6f0f10716f0ffc10c934c7bdb0db1f8f1d1
krikaz <pascal.bou@protonmail.com>
2019-07-24 14:00:10 +0200

    restricted mw now works

--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 const restricted = require('../auth/restricted-middleware');
-const checkRole = require('../auth/check-dept-middleware');
+const checkDept = require('../auth/check-dept-middleware');
 const Users = require('./users-model.js');
 
-router.get('/', restricted, checkRole('Student'), (req, res) => {
+router.get('/', restricted, (req, res) => {
 	Users.find()
 		.then(users => {
 			res.json(users);
